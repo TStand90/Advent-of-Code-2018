82012bd451201f8709917105324d80d9c3c275d5
Tyler Standridge <tstand90@gmail.com>
2018-12-17 21:30:09 -0800

    Day 11 part 2 completed in Go

--- day_11/day_11.go
+++ day_11/day_11.go
@@ -42,16 +42,13 @@ func dayOne(serialNumber int) {
 	for x := 0; x < len(powerLevels)-2; x++ {
 		for y := 0; y < len(powerLevels[x])-2; y++ {
 			value := 0
+			distance := 3
 
-			value += powerLevels[x][y]
-			value += powerLevels[x][y+1]
-			value += powerLevels[x][y+2]
-			value += powerLevels[x+1][y]
-			value += powerLevels[x+1][y+1]
-			value += powerLevels[x+1][y+2]
-			value += powerLevels[x+2][y]
-			value += powerLevels[x+2][y+1]
-			value += powerLevels[x+2][y+2]
+			for zx := 0; zx < distance; zx++ {
+				for zy := 0; zy < distance; zy++ {
+					value += powerLevels[x+zx][y+zy]
+				}
+			}
 
 			if value > highestValue {
 				highestValue = value
@@ -64,6 +61,49 @@ func dayOne(serialNumber int) {
 	fmt.Println("Coordinates:", highestXCoordinate+1, ",", highestYCoordinate+1)
 }
 
+func dayTwo(serialNumber int) {
+	powerLevels := make([][]int, 300)
+
+	for x := 1; x <= 300; x++ {
+		powerLevels[x-1] = make([]int, 300)
+
+		for y := 1; y <= 300; y++ {
+			powerLevels[x-1][y-1] = getPowerLevelForCoordinates(x, y, serialNumber)
+		}
+	}
+
+	highestXCoordinate := -1
+	highestYCoordinate := -1
+	highestSize := -1
+	highestValue := -100
+
+	for distance := 0; distance < len(powerLevels)-2; distance++ {
+		for x := 0; x < len(powerLevels)-distance; x++ {
+			for y := 0; y < len(powerLevels[x])-distance; y++ {
+				value := 0
+
+				if len(powerLevels)-distance < len(powerLevels) && len(powerLevels[x])-distance < len(powerLevels[x]) {
+					for zx := 0; zx <= distance; zx++ {
+						for zy := 0; zy <= distance; zy++ {
+							value += powerLevels[x+zx][y+zy]
+						}
+					}
+
+					if value > highestValue {
+						highestValue = value
+						highestXCoordinate = x
+						highestYCoordinate = y
+						highestSize = distance
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Println("Coordinates:", highestXCoordinate+1, ",", highestYCoordinate+1, ",", highestSize+1)
+}
+
 func main() {
 	dayOne(6303)
+	dayTwo(6303)
 }
